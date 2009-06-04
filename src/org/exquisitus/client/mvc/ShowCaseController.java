a45fac3abe3a24b2428d6303ae8522d7ed6a48e2
Gianluigi Davassi <davassi@yahoo.it>
2009-06-05 00:06:53 +0100

    first showcaseview creation

---
+++ src/org/exquisitus/client/mvc/ShowCaseController.java
@@ -0,0 +1,24 @@
+package org.exquisitus.client.mvc;
+
+import org.exquisitus.client.ApplicationEvents;
+
+import com.extjs.gxt.ui.client.mvc.AppEvent;
+import com.extjs.gxt.ui.client.mvc.Controller;
+
+public class ShowCaseController extends Controller {
+	
+	private ShowCaseView showCaseView = null;
+
+	public ShowCaseController() {
+		showCaseView = new ShowCaseView(this);
+		
+		registerEventTypes(ApplicationEvents.InitShowCaseEvent);
+	}
+
+	@Override
+	public void handleEvent(AppEvent event) {
+		
+
+	}
+
+}
---
+++ src/org/exquisitus/client/mvc/ShowCaseView.java
@@ -0,0 +1,20 @@
+package org.exquisitus.client.mvc;
+
+import com.extjs.gxt.ui.client.mvc.AppEvent;
+import com.extjs.gxt.ui.client.mvc.Controller;
+import com.extjs.gxt.ui.client.mvc.View;
+
+public class ShowCaseView extends View {
+
+	public ShowCaseView(Controller controller) {
+		super(controller);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void handleEvent(AppEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
