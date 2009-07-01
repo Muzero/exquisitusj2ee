f67fa052f58e05f33166b0392bd275e7d50d3fce
unknown <erenay@.(none)>
2009-07-01 22:20:59 +0100

    changing the content of test.java

--- test/org/exquisitus/test/test.java
+++ test/org/exquisitus/test/test.java
@@ -1,7 +1,13 @@
-package org.exquisitus.client;
+package org.exquisitus.test;
 
 // test class for my first push
 
 public class test {
 
+	public void doSomething(){
+		
+		//Do something very important :)
+		
+	}
+	
 }
